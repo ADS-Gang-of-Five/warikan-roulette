42ab6a73e8bbe3a1132923d5ce74526e57387552
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-12-03 21:31:18 +0900

    docs: TatekaeUseCase内のドキュメントコメントを修正

--- Roulette/Model/TatekaeUseCase.swift
+++ Roulette/Model/TatekaeUseCase.swift
@@ -15,12 +15,12 @@ struct TatekaeUsecase {
         self.tatekaeRepository = tatekaeRepository
     }
     
-    /// 指定したIDのメンバーを返す。
+    /// 指定したIDの立て替えを返す。
     func get(id: EntityID<Tatekae>) async throws -> Tatekae? {
         return try await tatekaeRepository.find(id: id)
     }
     
-    /// 指定した複数のIDのメンバーを返す。
+    /// 指定した複数のIDの立て替えを返す。
     func get(ids: [EntityID<Tatekae>]) async throws -> [Tatekae] {
         return try await tatekaeRepository.find(ids: ids)
     }

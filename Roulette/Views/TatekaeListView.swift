6c6c35c49fe7d6bc7ae3f0d26872174b0aa3475e
Masaki Doi <dwarf_s_w@yahoo.co.jp>
2023-12-06 11:55:34 +0900

    change: List内の要素をSectionで囲みheaderを付与した。コメントアウトされている余計なコードを削除した。

--- Roulette/Views/TatekaeListView.swift
+++ Roulette/Views/TatekaeListView.swift
@@ -7,71 +7,6 @@
 
 import SwiftUI
 
-//// 初期画面
-//
-//struct TatekaeListView: View {
-//    @EnvironmentObject var viewRouter: ViewRouter
-//
-//    var body: some View {
-//        NavigationStack(path: $viewRouter.path) {
-//            ZStack {
-//                Text("右下のボタンから立替を追加")
-//
-//                MyButton(diameter: 65)
-//            }
-//        }
-//        .toolbar {
-//            ToolbarItem(placement: .topBarTrailing) {
-//                Button("精算") {
-//
-//                }
-//                .bold()
-//            }
-//        }
-//    }
-//}
-//
-//private struct MyButton: View {
-//    let diameter: CGFloat
-//
-//    var body: some View {
-//        HStack {
-//            Spacer()
-//            VStack {
-//                Spacer()
-//                    Image(systemName: "plus.circle.fill")
-//                        .resizable()
-//                        .foregroundStyle(Color.blue)
-//                        .frame(width: diameter, height: diameter)
-//                        .background(.white)
-//
-//                .padding(.trailing)
-//            }
-//        }
-//    }
-//}
-//
-//#Preview {
-//    TatekaeListView()
-//        .environmentObject(ViewRouter())
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 struct TatekaeListView: View {
     @EnvironmentObject var viewRouter: ViewRouter
     @State var isShowAddTatekaeView = false
@@ -86,21 +21,25 @@ struct TatekaeListView: View {
         ZStack {
             if tatekaes != [] {
                 List {
-                    ForEach(tatekaes, id: \.self) { tatekae in
-                        HStack {
-                            Text(tatekae)
-                                .font(.title2)
-                            Spacer()
-                            VStack {
-                                Text("2023年11月14日")
-                                Text("合計 XXXXX円")
+                    Section {
+                        ForEach(tatekaes, id: \.self) { tatekae in
+                            HStack {
+                                Text(tatekae)
+                                    .font(.title2)
+                                Spacer()
+                                VStack {
+                                    Text("2023年11月14日")
+                                    Text("合計 XXXXX円")
+                                }
+                                .font(.footnote)
                             }
-                            .font(.footnote)
+                            .padding(.vertical, 3)
+                            .onTapGesture {
+                                isShowTatekaeDetailView = true
+                            }
                         }
-                        .padding(.vertical, 3)
-                        .onTapGesture {
-                            isShowTatekaeDetailView = true
-                        }
+                    } header: {
+                        Text("立替一覧")
                     }
                 }
             } else {

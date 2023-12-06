278dbd07d8499f6646f671293dcfb7d2512a9d5a
Masaki Doi <dwarf_s_w@yahoo.co.jp>
2023-12-06 10:18:59 +0900

    グループ作成前のViewとグループ作成後のViewを統合し、配列groupsが空か否かでViewが切り替わるようにした。

--- Roulette/Views/GroupListView.swift
+++ Roulette/Views/GroupListView.swift
@@ -7,105 +7,46 @@
 
 import SwiftUI
 
-
-//// 初期画面
-//
-//struct GroupListView: View {
-//    @StateObject var viewRouter = ViewRouter()
-//    @State var isShowAddGroupListView = false
-//    
-//    var body: some View {
-//        NavigationStack(path: $viewRouter.path) {
-////            List {
-//////                NavigationLink
-////            }
-////            .navigationTitle("割り勘グループ")
-////            .navigationDestination(for: Path.self) { path in
-////                switch path {
-////                case .addGroupView:
-////                    AddGroupListView()
-////                }
-////            }
-//            ZStack {
-//                Text("割り勘グループを右下のボタンから追加してください")
-//                    .font(.title2)
-//                    .padding(.horizontal, 30)
-//                
-//                MyButton(diameter: 65)
-//                    .onTapGesture {
-//                        isShowAddGroupListView = true
-//                    }
-//            }
-//            .navigationTitle("割り勘グループ")
-//        }
-//        .sheet(isPresented: $isShowAddGroupListView, content: {
-//            AddGroupListView(isShowAddGroupListView: $isShowAddGroupListView)
-//        })
-//    }
-//}
-//
-//private struct MyButton: View {
-//    let diameter: CGFloat
-//    
-//    var body: some View {
-//        HStack {
-//            Spacer()
-//            VStack {
-//                Spacer()
-//                    Image(systemName: "plus.circle.fill")
-//                        .resizable()
-//                        .foregroundStyle(Color.blue)
-//                        .frame(width: diameter, height: diameter)
-//                        .background(.white)
-//                
-//                .padding(.trailing)
-//            }
-//        }
-//    }
-//}
-//
-//#Preview {
-//    GroupListView()
-//}
-
-
-
-
-
-
-
-// すでに割り勘ループが作成されている場合
-
 struct GroupListView: View {
     @StateObject var viewRouter = ViewRouter()
     @State var isShowAddGroupListView = false
+    let groups: [String]
+    
+    init(groups: [String] = ["Gang of Five", "ひなっこクラブ", "アプリ道場サロン"]) {
+        self.groups = groups
+    }
     
     var body: some View {
         NavigationStack(path: $viewRouter.path) {
             ZStack {
-                List {
-                    NavigationLink("Gang of Five", value: Path.tatekaeListView)
-                    NavigationLink("ひなっこクラブ", value: Path.tatekaeListView)
+                if groups != [] {
+                        List {
+                            ForEach(groups, id: \.self) { group in
+                                NavigationLink(group, value: Path.tatekaeListView)
+                            }
+                        }
+                        .navigationDestination(for: Path.self) { path in
+                            switch path {
+                            case .tatekaeListView:
+                                TatekaeListView()
+                                    .navigationTitle("Gang of five")
+                            case .confirmView:
+                                ConfirmView()
+                                    .navigationTitle("立て替えの確認")
+                                    .navigationBarTitleDisplayMode(.inline)
+                            case .rouletteView:
+                                RouletteView()
+                            case .rouletteResultView:
+                                RouletteResultView()
+                            case .seisanResultView:
+                                SeisanResultView()
+                            }
+                        }
+                } else {
+                    Text("割り勘グループを右下のボタンから追加してください")
+                        .font(.title2)
+                        .padding(.horizontal, 30)
                 }
-                
-                .navigationDestination(for: Path.self) { path in
-                    switch path {
-                    case .tatekaeListView:
-                        TatekaeListView()
-                            .navigationTitle("Gang of five")
-                    case .confirmView:
-                        ConfirmView()
-                            .navigationTitle("立て替えの確認")
-                            .navigationBarTitleDisplayMode(.inline)
-                    case .rouletteView:
-                        RouletteView()
-                    case .rouletteResultView:
-                        RouletteResultView()
-                    case .seisanResultView:
-                        SeisanResultView()
-                    }
-                }
-                
                 MyButton(diameter: 65)
                     .onTapGesture {
                         isShowAddGroupListView = true
@@ -129,13 +70,12 @@ private struct MyButton: View {
             Spacer()
             VStack {
                 Spacer()
-                    Image(systemName: "plus.circle.fill")
-                        .resizable()
-                        .foregroundStyle(Color.blue)
-                        .frame(width: diameter, height: diameter)
-                        .background(.white)
-                
-                .padding(.trailing)
+                Image(systemName: "plus.circle.fill")
+                    .resizable()
+                    .foregroundStyle(Color.blue)
+                    .frame(width: diameter, height: diameter)
+                    .background(.white)
+                    .padding(.trailing)
             }
         }
     }
@@ -143,4 +83,5 @@ private struct MyButton: View {
 
 #Preview {
     GroupListView()
+//    GroupListView(groups: [])
 }

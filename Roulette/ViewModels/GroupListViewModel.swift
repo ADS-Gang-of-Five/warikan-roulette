6ba66b09b613a4a55c1e204a993107f5dbfbd811
sasasan03 <sako.hiro62@gmail.com>
2023-12-24 17:00:15 +0900

    add: GroupListViewModel内にメンバーとグループ名を追加するメソッドを追加

--- Roulette/ViewModels/GroupListViewModel.swift
+++ Roulette/ViewModels/GroupListViewModel.swift
@@ -24,13 +24,22 @@ final class GroupListViewModel: ObservableObject {
         )
     }
 
-    func fecthGroupAll() async {
+    func fecthAllGroups() async {
         print("fetchGroupAllが呼ばれました。")
         do {
             groups = try await warikanGroupUseCase.getAll()
             print("groups", groups)
         } catch {
             print(error)
+        }
+    }
+    
+    func createWarikanGroup(name: String, memberNames: [String]) async {
+        do {
+            try await warikanGroupUseCase.create(name: name, memberNames: memberNames)
+            await fecthAllGroups()
+        } catch {
+            print(#file, #line, "couldn't create")
         }
     }
 }

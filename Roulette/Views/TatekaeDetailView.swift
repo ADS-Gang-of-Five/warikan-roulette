b199feb064f14a4617f8460eaac7829bb578d619
sasasan03 <sako.hiro62@gmail.com>
2023-12-26 18:30:05 +0900

    change: Sheetを開くためのメソッドを変更。.task{ } の中にgetTatekaeListとgetMembersを使ってViewModelのプロパティに値を持たせ使用に変更

--- Roulette/Views/TatekaeDetailView.swift
+++ Roulette/Views/TatekaeDetailView.swift
@@ -7,48 +7,52 @@
 
 import SwiftUI
 
-struct TatekaeDetailView: View {
-    @Binding var isShowTatekaeDetailView: Bool
-    
-    var body: some View {
-        NavigationStack {
-            List {
-                Section {
-                    Text("朝食")
-                } header: {
-                    Text("立替の名目")
-                }
-                Section {
-                    Text("5,000円")
-                } header: {
-                    Text("立替の金額")
-                }
-                Section {
-                    Text("Maki")
-                } header: {
-                    Text("立替人")
-                }
-                Section {
-                    Text("2023年12月5日 10:00")
-                } header: {
-                    Text("日時")
-                }
-            }
-            .navigationTitle("立替の詳細")
-            .toolbarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button(action: {
-                        isShowTatekaeDetailView = false
-                    }, label: {
-                        Image(systemName: "xmark.circle")
-                    })
-                }
-            }
-        }
-    }
-}
+//struct TatekaeDetailView: View {
+ //    @Binding var isShowTatekaeDetailView: Bool
+ //    let tatekae: Tatekae
+ //
+ //    var body: some View {
+ //        NavigationStack {
+ //            List {
+ //                Section {
+ //                    Text(tatekae.name)
+ //                } header: {
+ //                    Text("立替の名目")
+ //                }
+ //                Section {
+ //                    Text("\(tatekae.money)円")
+ //                } header: {
+ //                    Text("立替の金額")
+ //                }
+ //                Section {
+ //                    Text("xxxxxxxx")
+ //                } header: {
+ //                    Text("立替人")
+ //                }
+ //                Section {
+ //                    Text("20xx年xx月xx日 10:00")
+ //                } header: {
+ //                    Text("日時")
+ //                }
+ //            }
+ //            .navigationTitle("立替の詳細")
+ //            .toolbarTitleDisplayMode(.inline)
+ //            .toolbar {
+ //                ToolbarItem(placement: .topBarTrailing) {
+ //                    Button(action: {
+ //                        isShowTatekaeDetailView = false
+ //                    }, label: {
+ //                        Image(systemName: "xmark.circle")
+ //                    })
+ //                }
+ //            }
+ //        }
+ //    }
+ //}
+ //
+ // swiftlint:disable comment_spacing
+ //#Preview {
+ //    TatekaeDetailView(isShowTatekaeDetailView: Binding.constant(true))
+ //}
+ // swiftlint:enable comment_spacing
 
-#Preview {
-    TatekaeDetailView(isShowTatekaeDetailView: Binding.constant(true))
-}

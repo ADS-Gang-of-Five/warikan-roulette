323a1e1da0cc30c4355ea94bed6a7c8bfad83574
sasasan03 <sako.hiro62@gmail.com>
2023-12-31 18:15:34 +0900

    change: @EnvironmetnObjectにprivateを付与。インデントを揃えた。

--- Roulette/Views/GroupListView.swift
+++ Roulette/Views/GroupListView.swift
@@ -10,36 +10,36 @@ import SwiftUI
 struct GroupListView: View {
     @StateObject private var viewRouter = ViewRouter()
     @State private var isShowAddGroupListView = false
-    @EnvironmentObject var mainViewModel: MainViewModel
-
+    @EnvironmentObject private var mainViewModel: MainViewModel
+    
     var body: some View {
         NavigationStack(path: $viewRouter.path) {
             ZStack {
                 if !mainViewModel.groups.isEmpty {
-                        List {
-                            ForEach(mainViewModel.groups) { group in
-                                NavigationLink(group.name, value: Path.tatekaeListView)
-                            }
+                    List {
+                        ForEach(mainViewModel.groups) { group in
+                            NavigationLink(group.name, value: Path.tatekaeListView)
                         }
-                        .navigationDestination(for: Path.self) { path in
-                            switch path {
-                            case .tatekaeListView:
-                                TatekaeListView()
-                                    .navigationTitle("groupName")
-                            case .confirmView:
-                                ConfirmView()
-                                    .navigationTitle("立て替えの確認")
-                                    .navigationBarTitleDisplayMode(.inline)
-                            case .rouletteView:
-                                RouletteView()
-                            case .rouletteResultView:
-                                RouletteResultView()
-                            case .seisanResultView:
-                                SeisanResultView()
-                                    .navigationTitle("精算結果")
-                                    .navigationBarTitleDisplayMode(.large)
-                            }
+                    }
+                    .navigationDestination(for: Path.self) { path in
+                        switch path {
+                        case .tatekaeListView:
+                            TatekaeListView()
+                                .navigationTitle("groupName")
+                        case .confirmView:
+                            ConfirmView()
+                                .navigationTitle("立て替えの確認")
+                                .navigationBarTitleDisplayMode(.inline)
+                        case .rouletteView:
+                            RouletteView()
+                        case .rouletteResultView:
+                            RouletteResultView()
+                        case .seisanResultView:
+                            SeisanResultView()
+                                .navigationTitle("精算結果")
+                                .navigationBarTitleDisplayMode(.large)
                         }
+                    }
                 } else {
                     Text("割り勘グループを右下のボタンから追加してください")
                         .font(.title2)
@@ -55,9 +55,7 @@ struct GroupListView: View {
         .environmentObject(viewRouter)
         .environmentObject(mainViewModel)
         .sheet(isPresented: $isShowAddGroupListView) {
-            AddGroupView(
-                isShowAddGroupListView: $isShowAddGroupListView
-            )
+            AddGroupView()
                 .interactiveDismissDisabled()
         }
         .task {

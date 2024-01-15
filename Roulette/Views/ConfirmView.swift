03ed806f90064f79707a152e1013051e149709e0
Masaki-JP-Fork <xxx@mail.com>
2024-01-15 16:57:52 +0900

    change: Viewの表示時にSeisanResponseを取得するよう変更

--- Roulette/Views/ConfirmView.swift
+++ Roulette/Views/ConfirmView.swift
@@ -14,68 +14,74 @@ struct ConfirmView: View {
         if let warikanGroup = mainViewModel.selectedGroup,
            let members = mainViewModel.selectedGroupMembers,
            let tatekaes = mainViewModel.selectedGroupTatekaes {
-            VStack(alignment: .leading) {
-                Text(warikanGroup.name)
-                    .font(.largeTitle)
-                    .fontWeight(Font.Weight.heavy)
-                Text("メンバー")
-                    .font(.callout)
-                    .padding(.top, 1)
-                HStack(spacing: nil) {
-                    ForEach(members) { member in
-                        Text(member.name)
-                    }
-                }
-                .font(.title2)
-                Text("立替一覧")
-                    .font(.callout)
-                    .padding(.top, 1)
-                Group {
-                    ForEach(tatekaes) { tatekae in
-                        HStack {
-                            Text(tatekae.name)
-                            Spacer()
-                            Text("\(tatekae.money)円")
+            VStack {
+                VStack(alignment: .leading) {
+                    Text(warikanGroup.name)
+                        .font(.largeTitle)
+                        .fontWeight(Font.Weight.heavy)
+                    Text("メンバー")
+                        .font(.callout)
+                        .padding(.top, 1)
+                    HStack(spacing: nil) {
+                        ForEach(members) { member in
+                            Text(member.name)
                         }
                     }
-                }
-                .font(.title2)
-                HStack {
-                    Text("合計金額")
-                    Spacer()
-                    let sum = tatekaes.reduce(0) { partialResult, tatekae in
-                        partialResult + tatekae.money
+                    .font(.title2)
+                    Text("立替一覧")
+                        .font(.callout)
+                        .padding(.top, 1)
+                    Group {
+                        ForEach(tatekaes) { tatekae in
+                            HStack {
+                                Text(tatekae.name)
+                                Spacer()
+                                Text("\(tatekae.money)円")
+                            }
+                        }
                     }
-                    Text("\(sum)円")
+                    .font(.title2)
+                    HStack {
+                        Text("合計金額")
+                        Spacer()
+                        let sum = tatekaes.reduce(0) { partialResult, tatekae in
+                            partialResult + tatekae.money
+                        }
+                        Text("\(sum)円")
+                    }
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding(.top, 1)
                 }
-                .font(.title)
-                .fontWeight(.semibold)
-                .padding(.top, 1)
+                switch mainViewModel.selectedGroupSeisanResponse {
+                case .needsUnluckyMember:
+                    NavigationLink("端数ルーレットする", value: Path.rouletteView)
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 20)
+                        .background(.blue)
+                        .clipShape(Capsule())
+                        .padding(.top)
+                case .success:
+                    NavigationLink("精算結果を見る", value: Path.seisanResultView)
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 20)
+                        .background(.blue)
+                        .clipShape(Capsule())
+                        .padding(.top)
+                case .none:
+                    Text("計算中...")
+                }
             }
             .padding(.horizontal, 50)
-            switch mainViewModel.selectedGroupSeisanResponse {
-            case .needsUnluckyMember:
-                NavigationLink("端数ルーレットする", value: Path.rouletteView)
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.white)
-                    .padding(.vertical, 10)
-                    .padding(.horizontal, 20)
-                    .background(.blue)
-                    .clipShape(Capsule())
-                    .padding(.top)
-            case .success:
-                NavigationLink("精算結果を見る", value: Path.seisanResultView)
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.white)
-                    .padding(.vertical, 10)
-                    .padding(.horizontal, 20)
-                    .background(.blue)
-                    .clipShape(Capsule())
-                    .padding(.top)
-            case .none:
-                Text("計算中...")
+            .task {
+                print("task")
+                await mainViewModel.getSeisanResponse()
             }
         } else {
             Text("エラーが発生しました。前の画面に一度戻り再度お試しください。")

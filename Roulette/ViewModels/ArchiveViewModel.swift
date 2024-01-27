e466912ba3a3991479f7354ec5f52fe8c816769f
sasasan03 <sako.hiro62@gmail.com>
2024-01-27 13:24:39 +0900

    feat: ArchivedViewModelにUsecaseを追加
    初期化の処理を追加
    ArchivedWarikanGroupsを取得してくるメソッドを追加

--- Roulette/ViewModels/ArchiveViewModel.swift
+++ Roulette/ViewModels/ArchiveViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+@MainActor
+final class ArchiveViewModel: ObservableObject {
+    @Published private(set) var archivedWarikanGroupsDataList: [ArchivedWarikanGroupData] = []
+    
+    private let archivedWarikanGroupUseCase: ArchivedWarikanGroupUseCase
+    private let memberUsecase: MemberUsecase
+    private let tatekaeUsecase: TatekaeUsecase
+    
+    init() {
+        let memberRepository = MemberRepository(userDefaultsKey: "member")
+        let memberUsecase = MemberUsecase(memberRepository: memberRepository)
+        
+        let tatekaeRepository = TatekaeRepository(userDefaultsKey: "tatekae")
+        let tatekaeUsecase = TatekaeUsecase(tatekaeRepository: tatekaeRepository)
+        
+        let archivedWarikanGroupRepository = ArchivedWarikanGroupRepository(userDefaultsKey: "archivedWarikanGroup")
+        let archivedWarikanGroupUseCase = ArchivedWarikanGroupUseCase(
+            archivedWarikanGroupRepository: archivedWarikanGroupRepository,
+            memberRepository: memberRepository
+        )
+        
+        self.archivedWarikanGroupUseCase = archivedWarikanGroupUseCase
+        self.memberUsecase = memberUsecase
+        self.tatekaeUsecase = tatekaeUsecase
+    }
+    
+    func getAllArchivedWarikanGroups() async {
+        do {
+            archivedWarikanGroupsDataList = try await archivedWarikanGroupUseCase.getAll()
+        } catch {
+            print(#function, error)
+        }
+    }
+}

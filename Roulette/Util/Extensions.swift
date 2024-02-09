a5ad73b46895c32e6d28229874b60d7e3138ea64
Masaki-JP <masakidayo0508@gmail.com>
2024-02-09 23:17:06 +0900

    feat: Date型に計算型プロパティstringを付与

--- Roulette/Util/Extensions.swift
+++ Roulette/Util/Extensions.swift
@@ -21,3 +21,19 @@ infix operator %%: MultiplicationPrecedence
 func %% (left: Int, right: Int) -> Int {
     ((left % right) + right) % right
 }
+
+/// 一時的な拡張
+/// OneView-OneViewModelに変更後削除
+/// Issue #139
+extension Date {
+    static let dateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.calendar = Calendar.autoupdatingCurrent
+        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm"
+        return dateFormatter
+    }()
+
+    var string: String {
+        return Self.dateFormatter.string(from: self)
+    }
+}

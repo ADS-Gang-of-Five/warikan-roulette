28039ec16e9b91a1b1653f3242742a02f45c5beb
Masaki-JP <masakidayo0508@gmail.com>
2024-02-10 22:22:45 +0900

    feat: TatekaeDetailViewModelを追加

--- Roulette/ViewModels/TatekaeDetailViewModel/TatekaeDetailViewModel.swift
+++ Roulette/ViewModels/TatekaeDetailViewModel/TatekaeDetailViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+@MainActor
+final class TatekaeDetailViewModel: ObservableObject {
+    private let tatekaeID: EntityID<Tatekae>
+    @Published private(set) var tatekaeDTO: TatekaeDTO?
+    private let tatekaeUseCase = TatekaeUseCase(tatekaeRepository: TatekaeRepository())
+
+    @Published var isShowAlert = false
+    @Published private(set) var alertText = ""
+
+    init(_ tatekaeID: EntityID<Tatekae>) {
+        self.tatekaeID = tatekaeID
+    }
+
+    func makeTatekaeDTO() async {
+        do {
+            if let tatekae = try await tatekaeUseCase.get(id: tatekaeID) {
+                self.tatekaeDTO = TatekaeDTO.convert(tatekae)
+            } else {
+                alertText = "データの読み込み中にエラーが発生しました。前の画面に戻って再度お試しください。"
+                isShowAlert = true
+            }
+        } catch {
+            print(error)
+            alertText = "データの読み込み中にエラーが発生しました。前の画面に戻って再度お試しください。"
+            isShowAlert = true
+        }
+    }
+}

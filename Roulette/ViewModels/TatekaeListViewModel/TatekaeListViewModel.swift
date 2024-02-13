cd20af83bd0a3c49b9d9f3af4025d6db04ce9176
Masaki-JP <masakidayo0508@gmail.com>
2024-02-13 17:48:25 +0900

    feat: TatekaeListViewModelの作成

--- Roulette/ViewModels/TatekaeListViewModel/TatekaeListViewModel.swift
+++ Roulette/ViewModels/TatekaeListViewModel/TatekaeListViewModel.swift
@@ -9,67 +9,44 @@ import Foundation
 
 @MainActor
  final class TatekaeListViewModel: ObservableObject {
-     @Published var tatekaes: [Tatekae] = []
-     @Published var members: [Member] = []
-     private let warikanGroupUseCase: WarikanGroupUseCase
-     private let memberUseCase: MemberUseCase
- 
-     init() {
-         self.memberUseCase = MemberUseCase(memberRepository: MemberRepository())
-         self.warikanGroupUseCase = WarikanGroupUseCase(
-             warikanGroupRepository: WarikanGroupRepository(),
-             memberRepository: MemberRepository(),
-             tatekaeRepository: TatekaeRepository()
-         )
-     }
- 
-     func getTatakaeList(id: EntityID<WarikanGroup>) async {
-         do {
-             tatekaes = try await warikanGroupUseCase.getTatekaeList(id: id)
-         } catch {
-//             print("error:", error)
-//             print(#file, #line)
+     let warikanGroupID: EntityID<WarikanGroup>
+     private let warikanGroupUseCase = WarikanGroupUseCase(
+        warikanGroupRepository: WarikanGroupRepository(),
+        memberRepository: MemberRepository(),
+        tatekaeRepository: TatekaeRepository()
+     )
+     private let tatekaeUseCase = TatekaeUseCase(tatekaeRepository: TatekaeRepository())
+
+     @Published private(set) var tatekaeDTOs: [TatekaeDTO]?
+
+     @Published var isShowAlert = false
+     @Published private(set) var alertText = ""
+
+     @Published var isShowAddTatekaeView = false
+     @Published private(set) var focusedTatekae: EntityID<Tatekae>?
+
+     var isNavigateToConfirmViewButtonDisabled: Bool {
+         return switch tatekaeDTOs {
+         case .some(let tatekaeDTOs) where tatekaeDTOs.count > 0: false
+         default: true
          }
      }
- 
-     func getMembers(ids: [EntityID<Member>]) async {
-         do {
-             members = try await memberUseCase.get(ids: ids)
-         } catch {
-             print("error:", error)
-             print(#file, #line)
-         }
+
+     init(warikanGroupID: EntityID<WarikanGroup>) {
+         self.warikanGroupID = warikanGroupID
      }
-     
-     func getMember(id: EntityID<Member>) async -> Member {
+
+     func makeTatekaeDTOs() async {
          do {
-             let member = try await memberUseCase.get(id: id)!
-             return member
+             let tatekaes = try await warikanGroupUseCase.getTatekaeList(id: warikanGroupID)
+             let dateFormatter = DateFormatter()
+             dateFormatter.calendar = Calendar.autoupdatingCurrent
+             dateFormatter.dateFormat = "yyyy/MM/dd HH:mm"
+             tatekaeDTOs = tatekaes.map { TatekaeDTO.convert($0, dateFormatter: dateFormatter) }
          } catch {
-             print("error:", error)
-             print(#file, #line)
-             fatalError()
-         }
-     }
- 
-     func appendTatekae(
-         warikanGroupID: EntityID<WarikanGroup>,
-         tatekaeName: String,
-         payerID: EntityID<Member>,
-         recipantIDs: [EntityID<Member>],
-         money: Int
-     ) async {
-         do {
-             try await warikanGroupUseCase.appendTatekae(
-                 warikanGroup: warikanGroupID,
-                 tatekaeName: tatekaeName,
-                 payer: payerID,
-                 recipants: recipantIDs,
-                 money: money
-             )
-         } catch {
-//             print("error:", error)
-//             print(#file, #line)
+             print(error)
+             alertText = "データの読み込み中にエラーが発生しました。前の画面に戻り再度お試しください。"
+             isShowAlert = true
          }
      }
  }

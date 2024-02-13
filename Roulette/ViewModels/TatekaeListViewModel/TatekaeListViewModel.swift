49993ad6d13f16575521038e9381f5c9de529a69
Masaki-JP <masakidayo0508@gmail.com>
2024-02-13 19:33:42 +0900

    change: TatekaeListViewModelのfocusedTatekaeDTOの型を変更

--- Roulette/ViewModels/TatekaeListViewModel/TatekaeListViewModel.swift
+++ Roulette/ViewModels/TatekaeListViewModel/TatekaeListViewModel.swift
@@ -23,7 +23,7 @@ import Foundation
      @Published private(set) var alertText = ""
 
      @Published var isShowAddTatekaeView = false
-     @Published var focusedTatekae: EntityID<Tatekae>?
+     @Published var focusedTatekaeDTO: TatekaeDTO?
 
      var isNavigateToConfirmViewButtonDisabled: Bool {
          return switch tatekaeDTOs {

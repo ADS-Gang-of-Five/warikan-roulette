4eeacd55c18c5e1bce59932d24b105a37ce9499d
Masaki-JP <masakidayo0508@gmail.com>
2024-02-22 21:27:15 +0900

    feat: GroupListViewModelを作成

--- Roulette/ViewModels/GroupListViewModel/GroupListViewModel.swift
+++ Roulette/ViewModels/GroupListViewModel/GroupListViewModel.swift
@@ -9,33 +9,43 @@ import Foundation
 
 @MainActor
 final class GroupListViewModel: ObservableObject {
-    @Published var groups: [WarikanGroup] = .init()
-    private let warikanGroupUseCase: WarikanGroupUseCase
+    @Published private(set) var warikanGroups: [WarikanGroup]? = []
+    @Published var isShowAddGroupView = false
+    @Published var isShowAlert = false
+    @Published private(set) var alertText = ""
+    @Published private var isDeletingGroup = false
+    var isNavigationLinkListDisabled: Bool { isDeletingGroup }
+    var isAddButtonDisabled: Bool { isDeletingGroup }
 
-    init() {
-        self.warikanGroupUseCase = WarikanGroupUseCase(
-            warikanGroupRepository: WarikanGroupRepository(),
-            memberRepository: MemberRepository(),
-            tatekaeRepository: TatekaeRepository()
-        )
-    }
+    private let warikanGroupUseCase = WarikanGroupUseCase(
+        warikanGroupRepository: WarikanGroupRepository(),
+        memberRepository: MemberRepository(),
+        tatekaeRepository: TatekaeRepository()
+    )
 
-    func fecthAllGroups() async {
-        print("fetchGroupAllが呼ばれました。")
+    func fetchAllWarikanGroups() async {
         do {
-            groups = try await warikanGroupUseCase.getAll()
-            print("groups", groups)
+            warikanGroups = try await warikanGroupUseCase.getAll()
         } catch {
             print(error)
+            alertText = "割り勘グループの取得に失敗しました。"
+            isShowAlert = true
         }
     }
-    
-    func createWarikanGroup(name: String, memberNames: [String]) async {
-        do {
-            try await warikanGroupUseCase.create(name: name, memberNames: memberNames)
-            await fecthAllGroups()
-        } catch {
-            print(#file, #line, "couldn't create")
+
+    func didTappedGroupDeleteButtonAction(id: EntityID<WarikanGroup>) {
+        guard isDeletingGroup == false else { return }
+        Task {
+            do {
+                isDeletingGroup = true
+                defer { isDeletingGroup = false }
+                try await warikanGroupUseCase.remove(ids: [id])
+                await fetchAllWarikanGroups()
+            } catch {
+                print(error)
+                alertText = "割り勘グループの削除に失敗しました。"
+                isShowAlert = true
+            }
         }
     }
 }

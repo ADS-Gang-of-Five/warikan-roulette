ba7d4a831c84c14237f8278afa5f783837350ee9
Masaki-JP <masakidayo0508@gmail.com>
2024-02-22 21:38:25 +0900

    change: GroupListViewがもつMainViewModelをGroupListViewModelに変更

--- Roulette/Views/GroupListView.swift
+++ Roulette/Views/GroupListView.swift
@@ -8,88 +8,68 @@
 import SwiftUI
 
 struct GroupListView: View {
+    @StateObject private var viewModel = GroupListViewModel()
     @StateObject private var viewRouter = ViewRouter()
-    @State private var isShowAddGroupView = false
-    @EnvironmentObject private var mainViewModel: MainViewModel
 
     var body: some View {
         NavigationStack(path: $viewRouter.path) {
-            VStack {
-                if !mainViewModel.allGroups.isEmpty {
-                    List(mainViewModel.allGroups) { group in
-                            HStack {
-                                Button(action: {
-                                    Task {
-                                        await mainViewModel.selectWarikanGroup(warikanGroup: group)
-                                        viewRouter.path.append(
-                                            Path.tatekaeListView(
-                                                id: group.id,
-                                                navigationTitle: group.name
-                                            )
-                                        )
-                                    }
-                                },
-                                       label: {
-                                    Text(group.name)
-                                        .foregroundStyle(.black)
-                                })
-                                Spacer()
-                                Image(systemName: "chevron.forward")
+            Group {
+                if let warikanGroups = viewModel.warikanGroups {
+                    List(warikanGroups) { warikanGroup in
+                        let path = Path.tatekaeListView(id: warikanGroup.id, navigationTitle: warikanGroup.name)
+                        NavigationLink(warikanGroup.name, value: path)
+                            .swipeActions(allowsFullSwipe: false) {
+                                Button("Delete", systemImage: "trash.fill", role: .destructive) {
+                                    viewModel.didTappedGroupDeleteButtonAction(id: warikanGroup.id)
+                                }.tint(.red)
                             }
                     }
-                    .navigationDestination(for: Path.self) { path in
-                        switch path {
-                        case .tatekaeListView(let id, let navigationTitle):
-                            TatekaeListView(warikanGroupID: id)
-                                .navigationTitle(navigationTitle)
-                        case .confirmView:
-                            ConfirmView()
-                                .navigationTitle("立て替えの確認")
-                                .navigationBarTitleDisplayMode(.inline)
-                        case .rouletteView:
-                            RouletteView()
-                        case .rouletteResultView(let id):
-                            RouletteResultView(archivedWarikanGroupID: id)
-                        case .seisanResultView(let archivedWarikanGroupID):
-                            SeisanResultView(archivedWarikanGroupID: archivedWarikanGroupID)
-                                .navigationTitle("精算結果")
-                                .navigationBarTitleDisplayMode(.large)
+                    .disabled(viewModel.isNavigationLinkListDisabled)
+                    .overlay {
+                        if warikanGroups.isEmpty {
+                            Text("割り勘グループを右下のボタンから追加してください。")
+                                .padding(.horizontal)
                         }
                     }
-                } else {
-                    Text("割り勘グループを右下のボタンから追加してください")
-                        .font(.title2)
-                        .padding(.horizontal, 30)
                 }
-            }
-            .task {
-                await mainViewModel.getAllWarikanGroups()
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .overlay(alignment: .bottomTrailing) {
-                AddButton {
-                    isShowAddGroupView = true
-                }
-                .padding(.trailing)
             }
             .navigationTitle("割り勘グループ")
+            .task { await viewModel.fetchAllWarikanGroups() }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay(alignment: .bottomTrailing) {
+                AddButton { viewModel.isShowAddGroupView = true }
+                    .disabled(viewModel.isAddButtonDisabled)
+                    .padding(.trailing)
+            }
+            .sheet(
+                isPresented: $viewModel.isShowAddGroupView,
+                onDismiss: { Task { await viewModel.fetchAllWarikanGroups() }},
+                content: { AddGroupView().interactiveDismissDisabled() }
+            )    
+            .navigationDestination(for: Path.self) { path in
+                switch path {
+                case .tatekaeListView(let id, let navigationTitle):
+                    TatekaeListView(warikanGroupID: id)
+                        .navigationTitle(navigationTitle)
+                case .confirmView:
+                    ConfirmView()
+                        .navigationTitle("立て替えの確認")
+                        .navigationBarTitleDisplayMode(.inline)
+                case .rouletteView:
+                    RouletteView()
+                case .rouletteResultView(let id):
+                    RouletteResultView(archivedWarikanGroupID: id)
+                case .seisanResultView(let archivedWarikanGroupID):
+                    SeisanResultView(archivedWarikanGroupID: archivedWarikanGroupID)
+                        .navigationTitle("精算結果")
+                        .navigationBarTitleDisplayMode(.large)
+                }
+            }
         }
         .environmentObject(viewRouter)
-        .environmentObject(mainViewModel)
-        .sheet(isPresented: $isShowAddGroupView,
-               onDismiss: {
-            Task {
-                await mainViewModel.getAllWarikanGroups()
-            }
-        },
-               content: {
-            AddGroupView()
-                .interactiveDismissDisabled()
-        })
     }
 }
 
 #Preview {
     GroupListView()
-        .environmentObject(MainViewModel())
 }

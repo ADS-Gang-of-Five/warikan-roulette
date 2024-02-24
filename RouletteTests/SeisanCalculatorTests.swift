2d2721a8687226c40c846ed3ea9bdd0f9b748f83
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-02-24 23:44:48 +0900

    test: Refactor to use guard-case

--- RouletteTests/SeisanCalculatorTests.swift
+++ RouletteTests/SeisanCalculatorTests.swift
@@ -47,30 +47,28 @@ final class SeisanCalculatorTests: XCTestCase {
         let sako = members[0].id
         let seig = members[1].id
         let maki = members[2].id
-        let calculator = SeisanCalculator(
-            memberRepository: InMemoryMemberRepository(members: members)
-        )
-        
         let tatekaeList = [
             createTatekae(name: "昼飯代", payer: sako, recipients: [sako, seig, maki], money: 1200),
             createTatekae(name: "タクシー代", payer: seig, recipients: [sako, seig, maki], money: 900),
             createTatekae(name: "宿代", payer: maki, recipients: [sako, seig, maki], money: 1080)
         ]
+        let calculator = SeisanCalculator(
+            memberRepository: InMemoryMemberRepository(members: members)
+        )
         
         // 計算実行
         let response = try await calculator.seisan(tatekaeList: tatekaeList)
-        
-        switch response {
-        case .success(let seisanList):
-            let result = seisanList.descript(with: members)
-            XCTAssertEqual(result, """
+        guard case .success(let seisanList) = response else {
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(
+            seisanList.descript(with: members),
+            """
             霽月 -> さこ: 140円
             霽月 -> まき: 20円
-            """)
-            
-        default:
-            XCTFail()
-        }
+            """
+        )
     }
     
     func test_case02_needsUnluckyMember() async throws {
@@ -82,35 +80,32 @@ final class SeisanCalculatorTests: XCTestCase {
         let sako = members[0].id
         let seig = members[1].id
         let maki = members[2].id
-        let calculator = SeisanCalculator(
-            memberRepository: InMemoryMemberRepository(members: members)
-        )
-        
         let tatekaeList = [
             createTatekae(name: "昼飯代", payer: sako, recipients: [sako, seig, maki], money: 1200),
             createTatekae(name: "タクシー代", payer: seig, recipients: [sako, seig, maki], money: 900),
             createTatekae(name: "宿代", payer: maki, recipients: [sako, seig, maki], money: 1100)
         ]
+        let calculator = SeisanCalculator(
+            memberRepository: InMemoryMemberRepository(members: members)
+        )
         
         // 計算実行
         let response = try await calculator.seisan(tatekaeList: tatekaeList)
+        guard case .needsUnluckyMember(let context) = response else {
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(Set(context.unluckyMemberCandidates), Set([sako, seig, maki]))
         
-        switch response {
-        case .needsUnluckyMember(let context):
-            XCTAssertEqual(Set(context.unluckyMemberCandidates), Set([sako, seig, maki]))
-            
-            // アンラッキーメンバーを指定して計算続行
-            let seisanList = try await calculator.seisan(context: context, unluckyMember: seig)
-            
-            let result = seisanList.descript(with: members)
-            XCTAssertEqual(result, """
+        // アンラッキーメンバーを指定して計算続行
+        let seisanList = try await calculator.seisan(context: context, unluckyMember: seig)
+        XCTAssertEqual(
+            seisanList.descript(with: members),
+            """
             霽月 -> さこ: 140円
             霽月 -> まき: 40円
-            """)
-            
-        default:
-            XCTFail()
-        }
+            """
+        )
     }
     
     func test_case03_アンラッキーメンバー候補() async throws {
@@ -124,29 +119,26 @@ final class SeisanCalculatorTests: XCTestCase {
         let calculator = SeisanCalculator(
             memberRepository: InMemoryMemberRepository(members: members)
         )
-        
         let tatekaeList = [
             createTatekae(name: "昼飯代", payer: seig, recipients: [sako, seig], money: 1234)
         ]
         
         // 計算実行
         let response = try await calculator.seisan(tatekaeList: tatekaeList)
-        
-        switch response {
-        case .needsUnluckyMember(let context):
-            XCTAssertEqual(Set(context.unluckyMemberCandidates), Set([sako, seig]))
-            
-            // アンラッキーメンバーを指定して計算続行
-            let seisanList = try await calculator.seisan(context: context, unluckyMember: sako)
-            
-            let result = seisanList.descript(with: members)
-            XCTAssertEqual(result, """
-            さこ -> 霽月: 620円
-            """)
-            
-        default:
+        guard case .needsUnluckyMember(let context) = response else {
             XCTFail()
+            return
         }
+        XCTAssertEqual(Set(context.unluckyMemberCandidates), Set([sako, seig]))
+        
+        // アンラッキーメンバーを指定して計算続行
+        let seisanList = try await calculator.seisan(context: context, unluckyMember: sako)
+        XCTAssertEqual(
+            seisanList.descript(with: members),
+            """
+            さこ -> 霽月: 620円
+            """
+        )
     }
     
     func test_case04_清算が不要の場合は空配列を返す() async throws {
@@ -158,26 +150,22 @@ final class SeisanCalculatorTests: XCTestCase {
         let sako = members[0].id
         let seig = members[1].id
         let maki = members[2].id
-        let calculator = SeisanCalculator(
-            memberRepository: InMemoryMemberRepository(members: members)
-        )
-        
         let tatekaeList = [
             createTatekae(name: "昼飯代", payer: sako, recipients: [sako, seig, maki], money: 1000),
             createTatekae(name: "タクシー代", payer: seig, recipients: [sako, seig], money: 667),
             createTatekae(name: "宿代", payer: maki, recipients: [sako, maki], money: 667)
         ]
+        let calculator = SeisanCalculator(
+            memberRepository: InMemoryMemberRepository(members: members)
+        )
         
         // 計算実行
         let response = try await calculator.seisan(tatekaeList: tatekaeList)
-        
-        switch response {
-        case .success(let seisanList):
-            XCTAssertTrue(seisanList.isEmpty)
-            
-        default:
+        guard case .success(let seisanList) = response else {
             XCTFail()
+            return
         }
+        XCTAssertTrue(seisanList.isEmpty)
     }
 
 }

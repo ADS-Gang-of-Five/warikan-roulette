65005515a0e8c62d316fbcfccec4d89279c594db
Masaki-JP <masakidayo0508@gmail.com>
2024-03-03 15:12:46 +0900

    feat: RouletteViewModelを作成

--- Roulette/ViewModels/RouletteViewModel/RouletteViewModel.swift
+++ Roulette/ViewModels/RouletteViewModel/RouletteViewModel.swift
@@ -5,4 +5,96 @@
 //  Created by Masaki Doi on 2023/12/07.
 //
 
-import Foundation
+import SwiftUI
+
+@MainActor
+final class RouletteViewModel: ObservableObject {
+    private let warikanGroupUseCase = WarikanGroupUseCase(
+        warikanGroupRepository: WarikanGroupRepository(),
+        memberRepository: MemberRepository(),
+        tatekaeRepository: TatekaeRepository()
+    )
+    private let memberUseCase = MemberUseCase(memberRepository: MemberRepository())
+    private let tatekaeUseCase = TatekaeUseCase(tatekaeRepository: TatekaeRepository())
+    private let seisanCalculator = SeisanCalculator(memberRepository: MemberRepository())
+    private let archiveController = WarikanGroupArchiveController(
+        warikanGroupRepository: WarikanGroupRepository(),
+        archivedWarikanGroupRepository: ArchivedWarikanGroupRepository()
+    )
+
+    @Published var angle = Angle(degrees: 0.0) // 見直し
+    @Published var isRouletteBottanTap = false // 見直し
+
+    @Published private(set) var members: [Member]?
+    @Published private(set) var tatekaeList: [Tatekae]?
+    let warikanGroupID: EntityID<WarikanGroup>
+    private(set) var archivedWarikanGroupID: EntityID<ArchivedWarikanGroup>?
+
+    @Published var isShowAlert = false
+    @Published private(set) var alertText = ""
+
+    init(warikanGroupID: EntityID<WarikanGroup>) {
+        self.warikanGroupID = warikanGroupID
+    }
+
+    func onAppearAction() async {
+        do {
+            try await getMembersAndTatekaeList()
+        } catch {
+            print(error)
+            alertText = "データの読み込みに失敗しました。前の画面に戻りもう一度お試しください。"
+            isShowAlert = true
+        }
+    }
+
+    private func getMembersAndTatekaeList() async throws {
+        let allWarikanGroups = try await warikanGroupUseCase.getAll()
+        guard let warikanGroup = allWarikanGroups.first(where: { warikanGroup in
+            warikanGroup.id == self.warikanGroupID
+        }) else { throw NSError(domain: "not found", code: 404) }
+        self.members = try await memberUseCase.get(ids: warikanGroup.members)
+        self.tatekaeList = try await tatekaeUseCase.get(ids: warikanGroup.tatekaeList)
+    }
+
+    private func archiveWarikanGroup() {}
+
+    func stopAtMember(name: String) {
+        guard let members else { return }
+        guard let selectedMemberIndex = members.firstIndex(where: { $0.name == name }) else { return }
+        let degreesPerMember = 360.0 / Double(members.count)
+        let halfSector = degreesPerMember / 2.0
+        let targetDegrees = degreesPerMember * Double(selectedMemberIndex) + halfSector
+        angle = .zero
+        withAnimation(.spring(duration: 10)) {
+            let extraRotation = 360.0 * 5
+            angle = .degrees(extraRotation - targetDegrees)
+        }
+    }
+
+    func didStartButtonTappedAction() {
+        isRouletteBottanTap = true
+        guard let unluckyMember = members?.randomElement() else {
+            fatalError() // FIXME: fatalError()
+        }
+        stopAtMember(name: unluckyMember.name)
+        Task {
+            // seinsan & archive
+            do {
+                guard let tatekaeList else { fatalError() } // FIXME: fatalError()
+                let seisanResponse = try await seisanCalculator.seisan(tatekaeList: tatekaeList)
+                guard case .success(let seisanDataList) = seisanResponse else {
+                    fatalError() // FIXME: fatalError()
+                }
+                archivedWarikanGroupID = try await archiveController.archive(
+                    id: warikanGroupID,
+                    seisanList: seisanDataList,
+                    unluckyMember: unluckyMember.id
+                )
+            } catch {
+                print(error)
+                alertText = "エラーが発生しました。前の画面に戻りもう一度お試しください。"
+                isShowAlert = true
+            }
+        }
+    }
+}
